e1b6468411c0d691ceb3e78fc5c749621b8ca392
Onur Sinan Güler <guler@onursinan.com>
2024-04-09 06:24:53 +0000

    Boid rendering with swing, vector type and base of the project

---
+++ app/src/main/java/com/onursinan/Main.java
@@ -0,0 +1,11 @@
+/*
+ * Boids flocking simulation by Craig Reynolds:
+ * https://www.red3d.com/cwr/boids/
+ */
+package com.onursinan;
+
+public class Main {
+    public static void main(String[] args) {
+        new SimulationFrame();
+    }
+}
---
+++ app/src/main/java/com/onursinan/SimulationRenderer.java
@@ -0,0 +1,53 @@
+package com.onursinan;
+
+import javax.swing.*;
+import javax.swing.Timer;
+import java.awt.*;
+import java.util.*;
+
+// TODO: Comment
+public class SimulationRenderer extends JPanel{
+    private LinkedList<Boid> boids = new LinkedList<Boid>();
+    private final int WIDTH = 1000;
+    private final int HEIGHT = 1000;
+    // Game Tick
+    private final Timer timer = new Timer(10, listener->repaint());
+
+    SimulationRenderer(){
+        // TODO: Initialize boids or generate in some way, and delete the test boids
+        boids.add(new Boid(100, 200, 5, -2));
+        boids.add(new Boid(200 , 700, -3, 7));
+        this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+    }
+    public void renderLoop(Graphics graphics){
+        Graphics2D g = (Graphics2D) graphics;
+        // Enable AA
+        // g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        paintBoids(g);
+    }
+    private void paintBoids(Graphics2D g){
+        boids.forEach(boid -> boid.updateBoid(g));
+    }
+
+    public void run(){
+        timer.start();
+    }
+
+    public void stop(){
+        timer.stop();
+    }
+
+    /* I do get the appeal, but java devs this is actuallly restarted.
+     * Understanding java swing in detail, took me longer than learning SDL
+     * and OpenGL combined. Maybe it's language familiarity, but I doubt it.
+     */
+    @Override
+    protected void paintComponent(Graphics graphics) {
+        super.paintComponent(graphics);
+        Graphics2D g = (Graphics2D)graphics;
+        g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+        renderLoop(graphics);
+    }
+
+}
