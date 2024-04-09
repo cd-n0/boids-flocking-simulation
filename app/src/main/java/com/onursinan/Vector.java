949af0c2f6b7f1ddc0772624eed6735779e6ae39
Onur Sinan Güler <guler@onursinan.com>
2024-04-09 09:49:00 +0000

    Alignment, better velocity limit and hue correction

--- app/src/main/java/com/onursinan/Vector.java
+++ app/src/main/java/com/onursinan/Vector.java
@@ -22,9 +22,44 @@ public class Vector {
         this.y = y;
     }
 
+    public double magnitude() {
+        return Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2));
+    }
+
+    public void normalize() {
+        double magnitude = magnitude();
+        if (magnitude != 0) {
+            x /= magnitude;
+            y /= magnitude;
+        }
+    }
+
+    public void limit(double limit){
+        double magnitude = this.magnitude();
+        if (magnitude > limit){
+            this.x *= limit / magnitude;
+            this.y *= limit / magnitude;
+        }
+    }
+
     public void add(Vector vector){
         this.x += vector.x;
         this.y += vector.y;
+    }
+
+    public void sub(Vector vector){
+        this.x -= vector.x;
+        this.y -= vector.y;
+    }
+
+    public void div(double value){
+        this.x /= value;
+        this.y /= value;
+    }
+
+    // Pythagorean theorem
+    public static double distance(Vector a, Vector b){
+        return Math.sqrt(Math.pow(a.x - b.x, 2) + Math.pow(a.y - b.y, 2));
     }
 
     // https://en.wikipedia.org/wiki/Atan2#/media/File:Atan2definition.svg
