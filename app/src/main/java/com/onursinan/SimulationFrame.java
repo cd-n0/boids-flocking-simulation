5714a33bea3ab08896223bcab3496319a2567d31
Onur Sinan Güler <guler@onursinan.com>
2024-04-11 01:24:44 +0000

    Better initialization, and removed JFrames setSize method since pack method is already taking care of it

--- app/src/main/java/com/onursinan/SimulationFrame.java
+++ app/src/main/java/com/onursinan/SimulationFrame.java
@@ -9,15 +9,11 @@ public class SimulationFrame extends JFrame{
 
     SimulationFrame(){
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        panel = new SimulationRenderer();
+        this.setVisible(true);
+        panel = new SimulationRenderer(this);
         this.add(panel, BorderLayout.CENTER);
         this.pack();
-        this.setSize(1000, 1000);
         this.setTitle("Boids Flocking Simulation");
         this.setLocation(0, 0);
-        this.setVisible(true);
-        Rectangle display = this.getBounds();
-        System.out.println(display.x + " " + display.y);
-        panel.run(this);
     }
 }
